a0dd577151e334d4f587a841e5af8985bc223ce8
Mike Bobiney <mikebobiney@gmail.com>
2011-04-15 23:02:25 -0400

    adding timer script

--- js/script.js
+++ js/script.js
@@ -1,26 +1,14 @@
-/* Author: 
 
-*/
-
+$(function() {
 
 
+});
 
+var func = function yothereman() {
+	console.log('asjdflaslfa');
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+_timer = new Timer(func);
 
 
 
