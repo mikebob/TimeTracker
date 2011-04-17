8c6841914504e9b63f84c091ee0945927ea838b2
Mike Bobiney <mikebobiney@gmail.com>
2011-04-17 13:40:06 -0400

    datamapper and sqlite db setup

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source :rubygems 
 
 gem "sinatra"
-gem "haml"
+gem "datamapper"
+gem "do_sqlite3"
--- app.rb
+++ app.rb
@@ -1,6 +1,60 @@
+require 'rubygems'
 require 'sinatra'
-require 'haml'
+require 'data_mapper'
+#require 'sqlite3'
+#require 'dm-core'
+#require 'dm-timestamps'
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/timetracker.db")
+
+class Entry
+
+	include DataMapper::Resource
+
+	property :id,			Serial
+	property :user_id,		String, :required => true
+	property :ticks,		String, :required => true
+	property :task_id,		String, :required => true
+	property :note,			Text
+	property :create_date,	DateTime, :default => Time.now
+	property :update_date,	DateTime, :default => Time.now
+
+	belongs_to :task
+	belongs_to :user
+
+end
+
+class User
+
+	include DataMapper::Resource
+
+	property :id,	Serial
+	property :name,	String, :required => true
+
+	has n, :entries
+
+end
+
+class Task
+
+	include DataMapper::Resource
+
+	property :id,	Serial
+	property :name,	Text, :required => true
+
+	has n, :entries
+
+end
+
+#create / upgrade all tables
+#DataMapper.auto_upgrade!
+DataMapper.auto_migrate!
 
 get '/' do
-	haml :hello
+	erb :fly
 end
+
+__END__
+
+@@ fly
+anything goes
